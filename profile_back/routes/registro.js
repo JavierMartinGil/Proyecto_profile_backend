a44196ddaf707f4d006cb4ab3b7d794631c9ed22
Javier Martin Gil <javiermartingil83@gmail.com>
2019-09-17 14:09:54 +0200

    Suprimir console.log

--- profile_back/routes/registro.js
+++ profile_back/routes/registro.js
@@ -6,13 +6,9 @@ const modelRegistro = require('../models/registro');
 
 
 router.post('/nuevo', (req, res) => {
-    console.log(req.body);
-    // req.body.password = bcrypt.hash(req.body.password, SECRET_KEY);
     modelRegistro.registro(req.body)
         .then((result) => {
-            console.log(result);
             res.json(result);
-
         })
         .catch((err) => {
             res.json({ error: err.message });
